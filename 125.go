2172865c3d009ca4f211379d902cde79f1b4bc3f
javlon <javlonrahimov1212@gmail.com>
2022-01-31 23:34:16 +0500

    1627

--- 125.go
+++ 125.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"strings"
 )
 
@@ -14,25 +14,24 @@ func IsPalindrome(s string) bool {
 	s = strings.ReplaceAll(s, " ", "")
 	processedString := strings.ToLower(s)
 
-	for i:=0; i<len(processedString)/2; i++ {
-        if processedString[i] != processedString[len(processedString)-1-i]{
-            return false
-        }
-    }
-    return true
+	for i := 0; i < len(processedString)/2; i++ {
+		if processedString[i] != processedString[len(processedString)-1-i] {
+			return false
+		}
+	}
+	return true
 }
 
-
 func strip_string(s string) string {
-    var result strings.Builder
-    for i := 0; i < len(s); i++ {
-        b := s[i]
-        if ('a' <= b && b <= 'z') ||
-            ('A' <= b && b <= 'Z') ||
-            ('0' <= b && b <= '9') ||
-            b == ' ' {
-            result.WriteByte(b)
-        }
-    }
-    return result.String()
+	var result strings.Builder
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if ('a' <= b && b <= 'z') ||
+			('A' <= b && b <= 'Z') ||
+			('0' <= b && b <= '9') ||
+			b == ' ' {
+			result.WriteByte(b)
+		}
+	}
+	return result.String()
 }
---
+++ 1627.go
@@ -0,0 +1,17 @@
+package main
+
+func MaximumWealth(accounts [][]int) int {
+
+	maxWealth := 0
+	for _, i := range accounts {
+		s := 0
+		for _, j := range i {
+			s += j
+		}
+		if s > maxWealth {
+			maxWealth = s
+		}
+	}
+
+	return maxWealth
+}
