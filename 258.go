7c159899e3ccc0d34dece758923a70bc074d5fb8
javlon <javlonrahimov1212@gmail.com>
2022-02-08 21:00:31 +0500

    258.go

---
+++ 258.go
@@ -0,0 +1,24 @@
+package main
+
+func AddDigits(num int) int {
+	seen := make(map[int]bool)
+	for  {
+		if _, ok := seen[num]; !ok {
+			seen[num] = true
+			num = digitSum(num)
+		} else {
+			break
+		}
+	}
+
+	return num
+}
+
+func digitSum(num int) int {
+	total := 0
+	for num > 0 {
+		total += num % 10
+		num /= 10
+	}
+	return total
+}
---
+++ 258_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test258(t *testing.T) {
+	cases := []struct {
+		Input int
+		Want int
+	} {
+		{Input: 38, Want: 2},
+		{Input: 0, Want: 0},
+	}
+
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%d number", test.Input), func(t *testing.T) {
+			got := AddDigits(test.Input)
+
+			if got != test.Want {
+				t.Errorf("got %d, want %d", got, test.Want)
+			}
+		})
+	}
+}
+
