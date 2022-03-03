236a166104fcd6c535ba3a978cb8d668263e88ba
javlon <javlonrahimov1212@gmail.com>
2022-03-03 09:32:29 +0500

    add 228 662

---
+++ 228.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func summaryRanges(nums []int) []string {
+	result := []string{}
+	if len(nums) == 0 {
+		return result
+	}
+	start := nums[0]
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i+1]-nums[i] != 1 || nums[i+1]-nums[i] != -1 {
+			if start != nums[i] {
+				result = append(result, fmt.Sprintf("%d->%d", start, nums[i]))
+			} else {
+				result = append(result, fmt.Sprintf("%d", start))
+			}
+			start = nums[i+1]
+			i++
+		}
+	}
+	return result
+}
