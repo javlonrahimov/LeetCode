a10e8873de63c15a4df4a8e388d5ebda887394ba
javlon <javlonrahimov1212@gmail.com>
2022-03-19 16:54:43 +0500

    add 33

---
+++ 33.go
@@ -0,0 +1,10 @@
+package main
+
+func search(nums []int, target int) int {
+    for index, value := range nums {
+        if value == target {
+            return index
+        }
+    }
+    return -1
+}
