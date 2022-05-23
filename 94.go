c3ba14fa4e54464eb4636690877d8e49b41ba37f
javlon <javlonrahimov1212@gmail.com>
2022-05-23 20:32:22 +0500

    add 94

--- 94.go
+++ 94.go
@@ -1,35 +1,21 @@
 package main
 
 
-type stack []TreeNode
-
-func (s stack) Push(v TreeNode) stack {
-    return append(s, v)
-}
-
-func (s stack) Pop() (stack, *TreeNode) {
-    // FIXME: What do we do if the stack is empty, though?
-
-    l := len(s)
-    if(l == 0) {
-        return nil, nil
-    }
-    return  s[:l-1], &s[l-1]
-}
-
 func inorderTraversal(root *TreeNode) []int {
- 	   res := make([]int, 0)
- 	   stack := make(stack, 0)
- 	   curr := root
- 	   for curr != nil || (len(stack) != 0) {
- 	   	for curr != nil {
- 	   		stack.Push(*curr)
- 	   		curr = curr.Left
- 	   	}
- 	   	 _, curr = stack.Pop()
- 	   	res = append(res, curr.Val)
- 	   	curr = curr.Right
- 	   }
- 	   return res
+    
+    ans := []int{}
+    
+    var inorder func(*TreeNode) 
+    
+    inorder = func(root *TreeNode) {
+        if root == nil {return}
+        
+        inorder(root.Left)
+        ans = append(ans, root.Val)
+        inorder(root.Right)
+        
+    }
+    inorder(root)
+    
+    return ans;   
 }
-//todo complete
