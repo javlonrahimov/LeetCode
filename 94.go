ddc8bf1bde560570da827b5c4c80fe1bb59de65e
javlon <javlonrahimov1212@gmail.com>
2022-06-09 23:35:42 +0500

    add 108

--- 94.go
+++ 94.go
@@ -1,21 +1,22 @@
 package main
 
-
 func inorderTraversal(root *TreeNode) []int {
-    
-    ans := []int{}
-    
-    var inorder func(*TreeNode) 
-    
-    inorder = func(root *TreeNode) {
-        if root == nil {return}
-        
-        inorder(root.Left)
-        ans = append(ans, root.Val)
-        inorder(root.Right)
-        
-    }
-    inorder(root)
-    
-    return ans;   
+
+	ans := []int{}
+
+	var inorder func(*TreeNode)
+
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		inorder(root.Left)
+		ans = append(ans, root.Val)
+		inorder(root.Right)
+
+	}
+	inorder(root)
+
+	return ans
 }
