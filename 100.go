5d708d30e98ac33d684b99a56d176128766cb27e
javlon <javlonrahimov1212@gmail.com>
2022-06-11 14:33:36 +0500

    add 100

---
+++ 100.go
@@ -0,0 +1,17 @@
+package main
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+
+	if p == nil && q == nil {return true};
+
+	if q == nil || p == nil {return false};
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	isSameTree(p.Left, q.Left)
+	isSameTree(p.Right, q.Right)
+
+	return true
+}
--- 108.go
+++ 108.go
@@ -2,12 +2,11 @@ package main
 
 func sortedArrayToBST(nums []int) *TreeNode {
 	if len(nums) == 0 {
-        return nil
-    }
-    return &TreeNode{
-        Val: nums[len(nums)/2],
-        Left: sortedArrayToBST(nums[:len(nums)/2]),
-        Right: sortedArrayToBST(nums[len(nums)/2 + 1:]),
-    }
+		return nil
+	}
+	return &TreeNode{
+		Val:   nums[len(nums)/2],
+		Left:  sortedArrayToBST(nums[:len(nums)/2]),
+		Right: sortedArrayToBST(nums[len(nums)/2+1:]),
+	}
 }
-
