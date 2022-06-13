5288e58b79f27f544f6b47e7de591ec655dbfd07
javlon <javlonrahimov1212@gmail.com>
2022-06-13 19:47:56 +0500

    add 101

---
+++ 101.go
@@ -0,0 +1,16 @@
+package main
+
+func isSymmetric(root *TreeNode) bool {
+    return helper101(root, root)
+}
+
+func helper101(left *TreeNode, right *TreeNode) bool {
+    if left == nil && right == nil {
+        return true
+    }
+    if left == nil || right == nil {
+        return false
+    }
+    return left.Val == right.Val && helper101(left.Right, right.Left) && helper101(left.Left, right.Right)
+    
+}
