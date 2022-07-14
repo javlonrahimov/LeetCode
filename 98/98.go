bd2b5f69ed578f63cdcbb27c16b20151df792af2
Javlon <56770292+javlonrahimov@users.noreply.github.com>
2022-07-14 19:34:55 +0500

    add 98

---
+++ 98/98.go
@@ -0,0 +1,25 @@
+package main
+
+func isValidBST(root *TreeNode) bool {
+    return isValid(root, nil, nil)
+}
+
+func isValid(root, min, max *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+    
+    if root.Left != nil && 
+    (root.Val <= root.Left.Val ||  
+     (min != nil && min.Val >= root.Left.Val)) {
+        return false
+    }
+    
+    if root.Right != nil && 
+    (root.Val >= root.Right.Val || 
+     (max != nil && max.Val <= root.Right.Val)){
+        return false
+    }
+    
+    return isValid(root.Left, min, root) && isValid(root.Right, root, max)
+}
