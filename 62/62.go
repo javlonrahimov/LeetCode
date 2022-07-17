8af73bea184915b79fcbd665c15bc8f9cbea005c
Javlon <56770292+javlonrahimov@users.noreply.github.com>
2022-07-17 11:19:46 +0500

    add 746 589

---
+++ 62/62.go
@@ -0,0 +1,20 @@
+package main
+
+func uniquePaths(m int, n int) int {
+dp := make([][]int, m)
+    for i := range dp {
+        dp[i] = make([]int, n)
+    }
+    for i := 0; i < len(dp); i++ {
+        for j := 0; j < len(dp[0]); j++ {
+            dp[i][j] = 1
+        }
+    }
+    
+    for i := 1; i < len(dp); i++ {
+        for j := 1; j < len(dp[0]); j++ {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        }
+    }
+    return dp[m - 1][n - 1]
+}
